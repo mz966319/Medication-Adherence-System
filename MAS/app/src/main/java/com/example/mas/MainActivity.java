6ea0af3eed07b9639d1ce6a5ff192c962f84c174
Moaaz Baiumy <mz966319@dal.ca>
2020-02-09 14:32:55 -0400

    linked to firebase

--- MAS/app/src/main/java/com/example/mas/MainActivity.java
+++ MAS/app/src/main/java/com/example/mas/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Firebase connection success", Toast.LENGTH_LONG).show();
     }
     /*tmp code*/
     public void MoveToMedProfActivity(View view){
         Intent intent = new Intent(MainActivity.this, medicalProfile.class);
         startActivity(intent);
     }
-    public void MoveToMedAddMedActivity(View view){
+    /*public void MoveToMedAddMedActivity(View view){
         Intent intent = new Intent(MainActivity.this, AddMedicine.class);
         startActivity(intent);
-    }
+    }*/
     /*end of tmp code*/
 }
