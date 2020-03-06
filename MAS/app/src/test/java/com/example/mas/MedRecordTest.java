fe610b88d825c06ace99fc0725388faf8f762d7c
Yurunyun W ang <tracy@TB6BE.WPA.Dal.Ca>
2020-03-05 21:38:53 -0300

    [TEST] Improve the unit tests of MedRecord object since we plan to improve MedRecord - [ Tracy & Colin ]

--- MAS/app/src/test/java/com/example/mas/MedRecordTest.java
+++ MAS/app/src/test/java/com/example/mas/MedRecordTest.java
@@ -9,22 +9,29 @@ import static org.junit.Assert.*;
 public class MedRecordTest {
     @Test
     public void gettersTest(){
-        MedRecord medR1 = new MedRecord("user1","med1", LocalDate.now());
-
+        MedRecord medR1 = new MedRecord("user1","med1");
+        medR1.setDate(LocalDate.now().minusDays(1).toString());
+        medR1.setQuantity("50.0");
+        medR1.setTakenStatus(true);
         assertEquals("user1",medR1.getUsername());
         assertEquals("med1", medR1.getMed_name());
-        assertEquals(LocalDate.now(),medR1.getDate());
+        assertTrue(medR1.getQuantity().equals("50.0"));
+        assertTrue(medR1.getTakenStatus());
     }
 
     @Test
     public void settersTest(){
-        MedRecord medR1 = new MedRecord("user1","med1", LocalDate.now());
+        MedRecord medR1 = new MedRecord("user1","med1");
         medR1.setUsername("usertest");
         medR1.setMed_name("medtest");
-        medR1.setDate(LocalDate.now().minusDays(1));
+        medR1.setQuantity("40.26");
+        medR1.setTakenStatus(false);
+        medR1.setDate(LocalDate.now().minusDays(1).toString());
 
         assertEquals("usertest",medR1.getUsername());
         assertEquals("medtest", medR1.getMed_name());
-        assertEquals(LocalDate.now().minusDays(1),medR1.getDate());
+        assertEquals(LocalDate.now().minusDays(1).toString(),medR1.getDate());
+        assertTrue(medR1.getQuantity().equals("40.26"));
+        assertFalse(medR1.getTakenStatus());
     }
 }
